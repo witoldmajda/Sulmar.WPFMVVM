a1d182a0514480ee5f578270797fa1c20e593df0
Witold Majda <witoldmajda@outlook.com>
2017-11-08 16:46:25 +0100

    Dodanie Behaviors

---
+++ Sulmar.WPFMVVM.ShopPracz.WPFClient/Behaviors/ButtonBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+
+namespace Sulmar.WPFMVVM.ShopPracz.WPFClient.Behaviors
+{
+    public class ButtonBehavior : Behavior<Button>
+    {
+        protected override void OnAttached()
+        {
+            this.AssociatedObject.MouseEnter += AssociatedObject_MouseEnter;
+        }
+
+        private void AssociatedObject_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            this.AssociatedObject.Background = new System.Windows.Media.SolidColorBrush(Colors.Blue);
+        }
+    }
+}
