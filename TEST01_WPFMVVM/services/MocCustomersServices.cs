87628f597e15d733443ed76711034d22df3c6ca4
adminWM <adminWM@DESKTOP-DSGOPIN>
2018-03-21 15:00:00 +0100

    zmiany20180321g15

---
+++ TEST01_WPFMVVM/services/MocCustomersServices.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TEST01_WPFMVVM.interfaces;
+using TEST01_WPFMVVM.model;
+
+namespace TEST01_WPFMVVM.services
+{
+    public class MocCustomersServices : ICustomersService
+    {
+        private ICollection<CustomerModel> customers;
+
+        public MocCustomersServices()
+        {
+            customers = new List<CustomerModel>
+            {
+                //new CustomerModel {Id=1, Name = "Name01" },
+                //new CustomerModel {Id=2, Name = "Name02" },
+            };
+        }
+       
+        public void Add(CustomerModel customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<CustomerModel> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerModel Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerModel Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(CustomerModel customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
