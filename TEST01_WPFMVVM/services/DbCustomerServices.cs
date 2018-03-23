b98e6c145bb8531c17e794fa85c99fc3420e9ffc
Witek <Witek@DESKTOP-L5KJMIO>
2018-03-23 21:06:13 +0100

    ch03

--- TEST01_WPFMVVM/services/DbCustomerServices.cs
+++ TEST01_WPFMVVM/services/DbCustomerServices.cs
@@ -4,10 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TEST01_WPFMVVM.interfaces;
+using TEST01_WPFMVVM.model;
 
 namespace TEST01_WPFMVVM.services
 {
-    public class DbCustomerServices :ICustomersService
+    public class DbCustomerServices : ICustomersService
     {
+        public DbCustomerServices()
+        {
+        }
+
+        public void Add(CustomerModel customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<CustomerModel> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerModel Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomerModel Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(CustomerModel customer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
