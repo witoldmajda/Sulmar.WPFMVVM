399bd386729f7e86a78d47771773befe6b8c1da3
Witek <Witek@DESKTOP-L5KJMIO>
2018-03-23 21:00:36 +0100

    change 01

--- TEST01_WPFMVVM/services/DbCustomerServices.cs
+++ TEST01_WPFMVVM/services/DbCustomerServices.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TEST01_WPFMVVM.interfaces;
 
 namespace TEST01_WPFMVVM.services
 {
-    class DbCustomerServices
+    public class DbCustomerServices :ICustomersService
     {
     }
 }
